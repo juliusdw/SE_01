7449027303da2885c35a59990156dc96da54cfc9
jdew <mail@jdew.de>
2020-12-01 17:36:49 +0100

    got checking for empty space or right color working

--- beakers.py
+++ beakers.py
@@ -1,27 +1,50 @@
 # topmost liquid gets poured
 # can only enter beaker if ther is space or the same 
 
-beakers = ["0", "0", "0", "2", "1", "2", "2", "1", "1"]
+beakers = [["0", "0", "0"], ["2", "1", "2"], ["2", "1", "1"]]
 
 class Game:
     
     def __init__(self):
         print("test")
     def print_beakers(self, beakers):
-        print("Nr1" + str(beakers[0:3]))
-        print("Nr2" + str(beakers[3:6]))
-        print("Nr3" + str(beakers[6:9]))
+        for beaker in beakers:
+            print(beaker)
 
     def get_move_from(self):
         from_beaker = input("Which Beaker do you want to pour from!")
         print("pouring from beaker nr: " + from_beaker)
+        from_beaker = int(from_beaker)
+        from_beaker = from_beaker -1
         return from_beaker
     def get_move_to(self):
         to_beaker = input("Which beaker do you want to pour to?")
         print("pouring into beaker nr: " + to_beaker)
+        to_beaker = int(to_beaker)
+        to_beaker = to_beaker -1
         return to_beaker
 
-    def check_empty(self, current_to_beaker):
+    def check_empty(self, current_to_beaker, current_from_beaker):
+        f = int(current_to_beaker)
+        t = int(current_to_beaker)
+        if "0" in beakers[f]:
+            return True
+        else:
+            for m in beakers[f]:
+                if m == 0:
+                    return None
+                elif m != 0:
+                    for z in beakers[t]:
+                        if z == m:
+                            return True
+                        else:
+                            return False
+
+           
+            print ("there is no space in this beaker!")
+            return 
+
+
         
 
   #  def check_same_fluid(self, current_from_beaker, current_to_beaker):
@@ -35,7 +58,12 @@ g = Game()
 g.print_beakers(beakers)
 current_from_beaker = g.get_move_from()
 current_to_beaker = g.get_move_to()
-#g.check_empty(current_to_beaker)
+is_empty = g.check_empty(current_to_beaker, current_from_beaker)
+if is_empty == False:
+    print("there is no space try again!")
+    
+else:
+    print("there is space or the right color")
 #is_empty = g.check_empty(current_to_beaker)
 #if is_empty == False:
   #  print("The Beaker if Full!")
